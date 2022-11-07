8edf3382b4d34fe89cf85c37218c8c6d95549350
Marrlenns <marlenormonbaev@gmail.com>
2022-11-07 15:18:56 +0600

    4-7 hw

---
+++ afisha/urls.py
@@ -0,0 +1,35 @@
+"""month4kanat URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from main.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', welcome),
+    path('about_us/', about),
+    path('date_now/', date_),
+    path('films/', films),
+    path('directors/', directors),
+    path('films/<int:id>/', one_film),
+    path('director/<int:director_id>/films/', director_films),
+    path('films/create/', create_film),
+    path('director/create/', create_director),
+    path('register/', register_),
+    path('login/', login_),
+    path('logout/', logout_),
+    path('search/', search)
+]
---
+++ main/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=100)
+    producer = models.CharField(max_length=100)
+    rating = models.IntegerField(default=0, blank=True, null=True)
+    duration = models.FloatField(default=0)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL,
+                                 related_name='films', null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.director.name}"
